2300317ffa0e7e1053f78252c531bc268babfa60
K. M. Kroski <kroski@live.com>
2018-05-19 16:10:55 -0500

    Fix to console warnings

--- src/components/blocks/FooterBlock.vue
+++ src/components/blocks/FooterBlock.vue
@@ -1,23 +1,32 @@
 <template>
   <div>
-    <router-link class="mobile-next" :to="next">
+    <router-link
+      class="mobile-next"
+      v-if="next !== null"
+      :to="next">
       Next <i class="ion-chevron-right"></i>
     </router-link>
-    <router-link class="mobile-prev" :to="previous">
+
+    <router-link
+      class="mobile-prev"
+      v-if="previous !== null"
+      :to="previous">
       <i class="ion-chevron-left"></i> Prev
     </router-link>
 
     <footer class="footer">
       <nav class="navbar navbar-expand">
         <div class="container">
-          <router-link class="nav-link" :to="previous">
+          <router-link class="nav-link" v-if="previous !== null" :to="previous">
             <i class="ion-chevron-left"></i> Prev
           </router-link>
           <div class="nav-link" v-if="previous === null">
             &nbsp;
           </div>
+
           <router-link class="nav-link" to="/toc/01/">Table of Contents</router-link>
-          <router-link class="nav-link" :to="next">
+
+          <router-link class="nav-link" v-if="next !== null" :to="next">
             Next <i class="ion-chevron-right"></i>
           </router-link>
           <div class="nav-link" v-if="next === null">
